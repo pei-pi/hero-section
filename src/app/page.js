9250b1a5d31b769bd045731094e94f103a7f4ca2
peipi <2390574106@qq.com>
2024-06-24 14:57:24 +0800

    modify bg image

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[url('/images/bg3.png')] bg-cover bg-no-repeat">
+    <div className="min-h-screen bg-[url('/images/bg.png')] bg-cover bg-no-repeat">
       <Navbar />
       <Hero />
     </div>
